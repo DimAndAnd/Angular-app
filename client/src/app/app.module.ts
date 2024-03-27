05ea1da1c8e4de177ac018db99d2a433cec68f87
DimAndAnd <123008948+DimAndAnd@users.noreply.github.com>
2024-03-27 13:09:32 +0200

    included services

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { HomeComponent } from './home/home.component';
 import { TrainersComponent } from './trainers/trainers.component';
 import { ErrorComponent } from './error/error.component';
 import { UserModule } from './user/user.module';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import { ThemesListComponent } from './themes-list/themes-list.component'
 
 @NgModule({
   declarations: [
@@ -16,11 +19,14 @@ import { UserModule } from './user/user.module';
     HomeComponent,
     TrainersComponent,
     ErrorComponent,
+    PostsListComponent,
+    ThemesListComponent,
   ],
   imports: [
     BrowserModule,
     CoreModule,
     SharedModule, 
+    HttpClientModule,
     UserModule,
     AppRoutingModule,
   ],
